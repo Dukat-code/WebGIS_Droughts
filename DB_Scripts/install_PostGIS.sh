55b6c7572b1ad7c919814c034b6082917c168e39
gonzdav <David.GONZALEZ-SANCHEZ@ext.ec.europa.eu>
2025-11-13 15:49:45 +0100

    correction postgis scripts

--- DB_Scripts/install_PostGIS.sh
+++ DB_Scripts/install_PostGIS.sh
@@ -3,8 +3,8 @@
 # Update package lists
 sudo apt update
 
-# Install PostgreSQL and PostGIS
-sudo apt install -y postgresql postgresql-contrib postgis postgresql-15-postgis-3
+# Install PostgreSQL 14 and PostGIS 3
+sudo apt install -y postgresql-14 postgresql-contrib postgis postgresql-14-postgis-3
 
 # Restart PostgreSQL service
 sudo systemctl restart postgresql
